2416dff74c434e69c02822d03797d6a703ccbb94
Afif Defriant <afif@jojonomic.com>
2023-11-07 17:33:36 +0700

    update config

--- dist/helpers/rng.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const rng = (min, max, decimal = 1) => {
-    const str = (Math.random() * (max - min) + min).toFixed(decimal);
-    return parseFloat(str);
-};
-exports.default = rng;
-//# sourceMappingURL=rng.js.map
